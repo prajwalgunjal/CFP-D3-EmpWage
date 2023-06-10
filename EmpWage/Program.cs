48f87791bb455b5111a3e065632968fc3009bbec
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-10 11:23:42 +0530

    UC4 added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -2,32 +2,43 @@
 {
     internal class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 0;
         static void Main(string[] args)
         {
-            int IS_FULL_TIME=1;
-            int EMP_RATE_PER_HOUR = 20;
-            int IS_PART_TIME = 0;
+
             int empHr;
             int empWage;
-            Random random   = new Random(); 
+            Random random = new Random();
             int ispresent = random.Next(3);
-            if(ispresent == IS_FULL_TIME)
+
+            switch (ispresent)
             {
-                Console.WriteLine("Employee is present");
-                Console.WriteLine("FullTime");
-                empHr = 8;
+                case IS_FULL_TIME:
+                    {
+                        Console.WriteLine("Employee is present");
+                        Console.WriteLine("FullTime");
+                        empHr = 8;
+                        break;
+                    }
+
+                case IS_PART_TIME:
+                    {
+                        Console.WriteLine("Employee is present");
+                        Console.WriteLine("parttime");
+                        empHr = 4;
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Emoployee is not present : ");
+                        empHr = 0;
+                        break;
+                    }
             }
-            else if (ispresent==IS_PART_TIME){
-                Console.WriteLine("Employee is present");
-                Console.WriteLine("parttime");
-                empHr=4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is not present");
-                empHr = 0;
-            }
-            empWage=EMP_RATE_PER_HOUR * empHr;
+            empWage = EMP_RATE_PER_HOUR * empHr;
             Console.WriteLine($"Wage of the employee is :- {empWage}");
         }
     }
