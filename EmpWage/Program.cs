21e5d12b1c89b985b7e77e9bc8fe480de700f523
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-10 11:14:57 +0530

    UC3 Added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,14 +6,21 @@
         {
             int IS_FULL_TIME=1;
             int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 0;
             int empHr;
             int empWage;
             Random random   = new Random(); 
-            int ispresent = random.Next(2);
+            int ispresent = random.Next(3);
             if(ispresent == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                Console.WriteLine("FullTime");
                 empHr = 8;
+            }
+            else if (ispresent==IS_PART_TIME){
+                Console.WriteLine("Employee is present");
+                Console.WriteLine("parttime");
+                empHr=4;
             }
             else
             {
