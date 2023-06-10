4fe4c3b724078d1af323508f187edd5f19efec20
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-10 12:36:03 +0530

    uc5 added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,41 +5,47 @@
         public const int IS_FULL_TIME = 1;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_PART_TIME = 0;
+        public const int NO_OF_WORKING_DAYS = 2;
         static void Main(string[] args)
         {
 
             int empHr;
             int empWage;
-            Random random = new Random();
-            int ispresent = random.Next(3);
-
-            switch (ispresent)
+            int TotalEmpWage=0;
+            for (int i = 0; i < NO_OF_WORKING_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    {
-                        Console.WriteLine("Employee is present");
-                        Console.WriteLine("FullTime");
-                        empHr = 8;
-                        break;
-                    }
+                Random random = new Random();
+                int ispresent = random.Next(3);
 
-                case IS_PART_TIME:
-                    {
-                        Console.WriteLine("Employee is present");
-                        Console.WriteLine("parttime");
-                        empHr = 4;
-                        break;
-                    }
+                switch (ispresent)
+                {
+                    case IS_FULL_TIME:
+                        {
+                            Console.WriteLine("Employee is present");
+                            Console.WriteLine("FullTime");
+                            empHr = 8;
+                            break;
+                        }
 
-                default:
-                    {
-                        Console.WriteLine("Emoployee is not present : ");
-                        empHr = 0;
-                        break;
-                    }
+                    case IS_PART_TIME:
+                        {
+                            Console.WriteLine("Employee is present");
+                            Console.WriteLine("parttime");
+                            empHr = 4;
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Emoployee is not present : ");
+                            empHr = 0;
+                            break;
+                        }
+                }
+                empWage = EMP_RATE_PER_HOUR * empHr;
+                TotalEmpWage += empWage;
+                Console.WriteLine($"Wage of the employee is :- {empWage}");
             }
-            empWage = EMP_RATE_PER_HOUR * empHr;
-            Console.WriteLine($"Wage of the employee is :- {empWage}");
         }
     }
 }
