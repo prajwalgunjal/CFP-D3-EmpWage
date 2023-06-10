cb95a5a482cb2320827d66eff51838be2a0da872
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-10 10:42:45 +0530

    UC1 added

---
+++ EmpWage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random   = new Random(); 
+            int ispresent = random.Next(2);
+            if(ispresent == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+        }
+    }
+}
