be6120f35d7e0fa0653c4a390e5cf9d257f5f51a
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-10 10:50:18 +0530

    uc2 added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -4,16 +4,24 @@
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME=1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHr;
+            int empWage;
             Random random   = new Random(); 
             int ispresent = random.Next(2);
-            if(ispresent == 0)
+            if(ispresent == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                empHr = 8;
             }
             else
             {
                 Console.WriteLine("Employee is not present");
+                empHr = 0;
             }
+            empWage=EMP_RATE_PER_HOUR * empHr;
+            Console.WriteLine($"Wage of the employee is :- {empWage}");
         }
     }
 }
