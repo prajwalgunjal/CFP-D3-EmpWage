1d18670e2a5682ee76777bdfbc7d2712eabe5044
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-11 10:43:28 +0530

    UC8 added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,22 +5,22 @@
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_WAGE_PER_HOUR = 20;
-        public const int WORKING_DAYS_PER_MONTH = 20;
-        public const int TOTAL_WORKING_HOURS = 100;
-        public static int Computewage()
+        //public const int EMP_WAGE_PER_HOUR = 20;
+        //public const int WORKING_DAYS_PER_MONTH = 20;
+        //public const int TOTAL_WORKING_HOURS = 100;
+        public static int Computewage(string company ,int empRatePerHour , int numOfWorkingDays, int maxHoursPerMonth)
         {
             int empHrs;
             int empWagePerDay;
             int empWagePerMonth = 0;
             int totalWorkingDays = 0;
             int totalEmpHours = 0;
-            while (totalEmpHours <= TOTAL_WORKING_HOURS && totalWorkingDays < WORKING_DAYS_PER_MONTH)
+            while (totalEmpHours <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
-                double empCheck = random.Next(3);
-                switch ((int)empCheck)
+                int empCheck = random.Next(3);
+                switch (empCheck)
                 {
                     case IS_FULL_TIME:
                         Console.WriteLine("Full Time:");
@@ -36,7 +36,7 @@
                         break;
                 }
                 totalEmpHours += empHrs;
-                empWagePerDay = empHrs * EMP_WAGE_PER_HOUR;
+                empWagePerDay = empHrs * empRatePerHour;
                 empWagePerMonth += empWagePerDay;
                 Console.WriteLine("Employee Wage per Day: " + empWagePerDay);
             }
@@ -47,7 +47,8 @@
         
         static void Main(string[] args)
         {
-            Computewage();
+            Computewage("Dmart", 20,2,10);
+            Computewage("Elpro", 30,4,20);
         }
     }
 }
