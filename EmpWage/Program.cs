e2db3015535200cbbe77b4f14f0d9d6ac4ef6b06
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-11 10:59:51 +0530

    uc9 added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -8,14 +8,29 @@
         //public const int EMP_WAGE_PER_HOUR = 20;
         //public const int WORKING_DAYS_PER_MONTH = 20;
         //public const int TOTAL_WORKING_HOURS = 100;
-        public static int Computewage(string company ,int empRatePerHour , int numOfWorkingDays, int maxHoursPerMonth)
+        private string Company;
+        private int EmpWagePerHour;
+        private int WorkingDaysPerMonth;
+        private int TotalWorkingHours;
+        private int totalWage;
+
+
+        public Program(string Company, int EmpWagePerHour, int WorkingDaysPerMonth, int TotalWorkingHours)
+        {
+            this.Company = Company;
+            this.EmpWagePerHour = EmpWagePerHour;
+            this.WorkingDaysPerMonth = WorkingDaysPerMonth;
+            this.TotalWorkingHours = TotalWorkingHours;
+        }
+
+        public void Computewage()
         {
             int empHrs;
             int empWagePerDay;
             int empWagePerMonth = 0;
             int totalWorkingDays = 0;
             int totalEmpHours = 0;
-            while (totalEmpHours <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            while (totalEmpHours <= this.TotalWorkingHours && totalWorkingDays < this.WorkingDaysPerMonth)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -36,19 +51,19 @@
                         break;
                 }
                 totalEmpHours += empHrs;
-                empWagePerDay = empHrs * empRatePerHour;
+                empWagePerDay = empHrs * EmpWagePerHour;
                 empWagePerMonth += empWagePerDay;
                 Console.WriteLine("Employee Wage per Day: " + empWagePerDay);
             }
             Console.WriteLine("Emp Wage Per Month : " + empWagePerMonth);
-            return empWagePerMonth;
+           
         }
 
         
         static void Main(string[] args)
         {
-            Computewage("Dmart", 20,2,10);
-            Computewage("Elpro", 30,4,20);
+            Program program =  new Program("Dmart",20,20,10);
+            program.Computewage();
         }
     }
 }
